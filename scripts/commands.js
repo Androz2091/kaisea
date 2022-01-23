77e07d2b36bdb6307940f7f52abf31ddcabaac4d
Androz <androz2091@gmail.com>
2022-01-23 08:21:54 +0100

    Update commands.js

--- scripts/commands.js
+++ scripts/commands.js
@@ -183,18 +183,18 @@ const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN);
 (async () => {
     try {
         console.log('Started refreshing application (/) commands.');
+/*
 
         await rest.put(
             Routes.applicationGuildCommands(process.env.DISCORD_CLIENT_ID, '890617349068165180'),
             { body: commands }
         ).then(console.log);
-        
-/*
+*/
         await rest.put(
             Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
             { body: commands }
         ).then(console.log);
-*/
+
         console.log('Successfully reloaded application (/) commands.');
     } catch (error) {
         console.error(error);
